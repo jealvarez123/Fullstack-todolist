021e15a5201f51b56ffae6c20b9222beed229065
jealvarez123 <jealvarez123@gmail.com>
2018-04-17 14:45:32 -0500

    added both fields for task and description in the index.html file

---
+++ public/scripts/app.js
@@ -0,0 +1,28 @@
+console.log('Can I kick it');
+
+var $todoList;
+
+var allTodoList = [];
+
+$(document).ready(function(){
+
+  $todoList = $('#todoTarget');
+  $.ajax({
+    method: 'GET',
+    url: '/api/todoList',
+    success: on()
+  });
+  });
+var on = () => {
+  console.log('Ajax call');
+}
+
+
+$(document).ready(function () {
+    $('button').click(function () {
+      $('#todo').append("<ul>" + $("input[name=task]").val() +': ' + $("input[name=des]").val() +  " <a href='#' class='close' aria-hidden='true'>&times;</a></ul>");
+    });
+    $("body").on('click', '#todo a', function () {
+        $(this).closest("ul").remove();
+    });
+});
