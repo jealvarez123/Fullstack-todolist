a1204cf7123896cd1164d0379f4b10db66348d82
jealvarez123 <jealvarez123@gmail.com>
2018-04-17 13:00:30 -0500

    created a express template, added the html, js, and css files

---
+++ seed.js
@@ -0,0 +1,19 @@
+var db = require('./models');
+
+
+// task: String,
+// description: String
+
+var toDoList = [
+  {
+    task: 'Wash dishes',
+    description: 'wash all the dirty dishes before the wife gets home'
+  },
+  {
+    task: 'Make the bed',
+    description: 'Maybe I can leave a hint for her ;)'
+  },
+  {
+    task: 'Pre dinner',
+    description: 'Defrost the chicken, chop the potatoes and carrots.'
+  }
